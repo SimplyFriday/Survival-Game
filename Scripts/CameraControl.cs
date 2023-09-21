8087b5daf5ea1d219aca8eb7cdd5157d1e5c7f5f
Friday Mae Spangenberg <thegirlcalledfriday@gmail.com>
2023-09-20 20:27:53 -0400

    gitignore & fixed janky ass camera zoom

--- Scripts/CameraControl.cs
+++ Scripts/CameraControl.cs
@@ -1,22 +1,23 @@
 using Godot;
-public partial class CameraControl : Node3D 
+public partial class CameraControl : Node3D
 {
 	[Export]
-	public float Sensitivity {get; set;} = 1.0f;
+	public float Sensitivity { get; set; } = 1.0f;
 
 	[Export]
-	public int FovStep {get; set;} = 5;
-	
+	public float DistanceStep { get; set; } = 0.5f;
+
 	[Export]
-	public string FollowObjectPath {get; set;}
+	public string FollowObjectPath { get; set; }
 
 	// Constant to slow rotation down to a manageable speed, such that Sensitivity can be a single digit number
-	private float _rotationScaling  = 0.005f;
+	private float _rotationScaling = 0.0005f;
 
 	private Camera3D _camera;
 	private Node3D _desiredPosition;
 	private RayCast3D _collider;
 	private Node3D _followedNode;
+
 	public override void _Ready()
 	{
 		base._Ready();
@@ -25,6 +26,8 @@ public partial class CameraControl : Node3D
 		_camera = GetNode<Camera3D>("Camera");
 		_desiredPosition = GetNode<Node3D>("DesiredPosition");
 		_collider = GetNode<RayCast3D>("CameraCollider");
+
+		_desiredPosition.LookAt(Transform.Origin);
 	}
 
 	public override void _Process(double delta)
@@ -38,14 +41,14 @@ public partial class CameraControl : Node3D
 
 		if (_collider.IsColliding())
 		{
-			//GlobalTransform = new Transform(GlobalTransform.basis, SPATIAL_TO_MOVE_TO.GlobalTransform.origin);
-			_camera.GlobalTransform = new Transform3D(	_camera.GlobalTransform.Basis,
+			_camera.GlobalTransform = new Transform3D(_camera.GlobalTransform.Basis,
 														_camera.GlobalTransform.Origin.Lerp(
-															_collider.GetCollisionPoint(), 
+															_collider.GetCollisionPoint(),
 															0.1f));
-		} else 
+		}
+		else
 		{
-			_camera.GlobalTransform = new Transform3D(	_camera.GlobalTransform.Basis,
+			_camera.GlobalTransform = new Transform3D(_camera.GlobalTransform.Basis,
 														_desiredPosition.GlobalTransform.Origin);
 		}
 	}
@@ -54,42 +57,52 @@ public partial class CameraControl : Node3D
 	{
 		base._UnhandledInput(inputEvent);
 
-		if ( Input.IsActionJustPressed("unlock_camera"))
+		if (Input.IsActionJustPressed("unlock_camera"))
 		{
 			Input.MouseMode = Input.MouseModeEnum.Captured;
-		} 
-		else if (Input.IsActionPressed("unlock_camera"))
+		}
+		else if (!Input.IsActionPressed("unlock_camera"))
 		{
 			Input.MouseMode = Input.MouseModeEnum.Visible;
 		}
 
 		if (inputEvent is InputEventMouseMotion mouseEvent &&
-			Input.IsActionPressed("unlock_camera"))
+			Input.MouseMode == Input.MouseModeEnum.Captured)
 		{
-			RotateObjectLocal(	Vector3.Left, 
+			RotateObjectLocal(Vector3.Left,
 								mouseEvent.Relative.Y * _rotationScaling * Sensitivity);
 
-			Rotate(	Vector3.Down, 
+			Rotate(Vector3.Down,
 					mouseEvent.Relative.X * _rotationScaling * Sensitivity);
 
 			// Clamp rotation so that it can't go upside down or under the floor
-			Rotation = new Vector3(	Mathf.Clamp(Rotation.X, -1.4f, 0), 
-									Rotation.Y, 
+			Rotation = new Vector3(Mathf.Clamp(Rotation.X, -1.4f, 0),
+									Rotation.Y,
 									0);
 		}
 
-		if (Input.IsActionJustPressed("zoom_in")) 
+		if (Input.IsActionJustPressed("zoom_in"))
 		{
-			var cam = GetNode<Camera3D>("Camera");
-			var sanitized = Mathf.Clamp(cam.Fov -= FovStep, 40, 100);
-			cam.Fov = sanitized;
+			if (_desiredPosition.Transform.Origin.Y > 2)
+			{
+				_desiredPosition.Transform = new Transform3D(	_desiredPosition.Basis, 
+																new Vector3 (
+																	_desiredPosition.Transform.Origin.X, 
+																	_desiredPosition.Transform.Origin.Y - DistanceStep,
+																	_desiredPosition.Transform.Origin.Z));
+			}
 		}
 
 		if (Input.IsActionJustPressed("zoom_out"))
 		{
-			var cam = GetNode<Camera3D>("Camera");
-			var sanitized = Mathf.Clamp(cam.Fov += FovStep, 40, 100);
-			cam.Fov = sanitized;
+			if (_desiredPosition.Transform.Origin.Y < 12)
+			{
+				_desiredPosition.Transform = new Transform3D(	_desiredPosition.Basis, 
+																new Vector3 (
+																	_desiredPosition.Transform.Origin.X, 
+																	_desiredPosition.Transform.Origin.Y + DistanceStep,
+																	_desiredPosition.Transform.Origin.Z));
+			}
 		}
 	}
 }
